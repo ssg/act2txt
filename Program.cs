36f15d4b04996e6fabb08b4f97acf9351b255612
Sedat Kapanoglu <sedat@kapanoglu.com>
2021-11-19 10:27:36 -0800

    cleanup code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-const long maxLen = 772; // https://web.archive.org/web/20211101165406/https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/
+const long maxActFileLength = 772; // https://web.archive.org/web/20211101165406/https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/
 const int maxColors = 256;
 const int maxPaintDotNetColors = 96;
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
@@ -10,7 +10,8 @@ ExtraData? extraData = null;
 
 if (args.Length != 2)
 {
-    Console.WriteLine($"act2txt v{version} - convert Adobe Palette files to Paint.Net palette - (c) 2021 SSG");
+    Console.WriteLine($"act2txt v{version} - (c) 2021 SSG");
+    Console.WriteLine("Converts Adobe Photoshop ACT Palette files to Paint.NET palette TXT format");
     abort("Usage: act2txt inputfile outputfile");
 }
 
@@ -20,69 +21,67 @@ if (!inputFile.Exists)
     abort($"Input file not found");
 }
 
+var outputFile = new FileInfo(args[1]);
+if (outputFile.Exists)
+{
+    abort("Output file already exists");
+}
+
 long len = inputFile.Length;
 if (len < 3)
 {
-    abort("Input file too small");
+    abort("Input file is too small");
 }
 
-if (len % 3 != 0 || len > maxLen)
+if (len % 3 != 0 || len > maxActFileLength)
 {
     Console.WriteLine("Input file doesn't seem to be a valid ACT file, but anyway...");
 }
 
-int numColors;
-if (len == maxLen)
-{
-    numColors = maxColors;
-} 
-else
-{
-    numColors = ((int)len) / 3;
-}
+int numColors = len == maxActFileLength ? maxColors : ((int)len) / 3;
 Console.WriteLine($"Processing {numColors} colors");
 if (numColors > maxPaintDotNetColors)
 {
-    Console.WriteLine($"Paint.Net only supports {maxPaintDotNetColors}, it will ignore the rest, fyi");
+    Console.WriteLine($"Paint.NET only supports {maxPaintDotNetColors}, it will ignore the rest, fyi");
 }
 
 var inputStream = inputFile.OpenRead();
 var buffer = new byte[numColors * 3];
 
 inputStream.Read(buffer);
-if (len == maxLen)
+if (len == maxActFileLength)
+{
+    extraData = readExtraData();
+}
+inputStream.Close();
+
+var writer = File.CreateText(outputFile.FullName);
+writer.WriteLine($"; Created by act2txt v{version} - https://github.com/ssg/act2txt");
+if (extraData is not null)
+{
+    writer.WriteLine($"; ACT number of colors = {extraData?.NumColors}");
+    writer.WriteLine($"; ACT transparent color index = {extraData?.TransparentIndex}");
+}
+
+for (int i = 0; i < buffer.Length; i += 3)
+{
+    writer.WriteLine($"FF{buffer[i]:X2}{buffer[i + 1]:X2}{buffer[i + 2]:X2}");
+}
+writer.Close();
+
+Console.WriteLine($"Written output to {outputFile.FullName}");
+Environment.Exit(0);
+
+ExtraData readExtraData()
 {
     var tempBuffer = new byte[4];
-    inputStream.Read(tempBuffer);
-    extraData = new ExtraData()
+    inputStream.Read(tempBuffer); // guaranteed to succeed
+    return new ExtraData()
     {
         NumColors = tempBuffer[0] + (tempBuffer[1] << 8),
         TransparentIndex = tempBuffer[2] + (tempBuffer[3] << 16),
     };
 }
-inputStream.Close();
-
-if (File.Exists(args[1]))
-{
-    abort("Output file already exists");
-}
-
-var writer = File.CreateText(args[1]);
-writer.WriteLine($"; Created by act2txt v{version} - https://github.com/ssg/act2txt");
-if (extraData is not null)
-{
-    writer.WriteLine($"; ACT reported number of colors = {extraData?.NumColors}");
-    writer.WriteLine($"; ACT reported transparent color index = {extraData?.TransparentIndex}");
-}
-
-for (int i = 0; i < numColors; i++)
-{
-    int offset = i * 3;
-    writer.WriteLine($"FF{buffer[offset]:X2}{buffer[offset + 1]:X2}{buffer[offset + 2]:X2}");
-}
-writer.Close();
-
-Console.WriteLine($"Written output to {args[1]}");
 
 static void abort(string msg)
 {
