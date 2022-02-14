fcdea7f94ce39aa3b408e5890199fde8de1a9e1a
Sedat Kapanoglu <sedat@kapanoglu.com>
2022-02-13 19:26:18 -0800

    add unit tests

---
+++ test/act2txt.Tests/ActPaletteTest.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.    Framework;
+using System.Drawing;
+using System.Security.Cryptography;
+
+namespace act2txt.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class ActPaletteTest
+    {
+        private static readonly TestCaseData[] shortTestData = new TestCaseData[]
+        {
+            new TestCaseData(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 })
+            {
+                ExpectedResult = new Color[] { 
+                    Color.FromArgb(1, 2, 3), 
+                    Color.FromArgb(4, 5, 6),
+                    Color.FromArgb(7, 8, 9),
+                    Color.FromArgb(10, 11, 12),
+                    Color.FromArgb(13, 14, 15),
+                },
+            },
+            new TestCaseData(Array.Empty<byte>())
+            {
+                ExpectedResult = Array.Empty<Color>(),
+            },
+            new TestCaseData(new byte[] { 1, 2, 3 })
+            {
+                ExpectedResult = new Color[] {
+                    Color.FromArgb(1, 2, 3),
+                },
+            },
+
+        };
+
+        [Test]
+        [TestCaseSource(nameof(shortTestData))]
+        public IReadOnlyList<Color> Ctor_ShorterFile_ReturnsExpectedColors(byte[] buffer)
+        {
+            var pal = new ActPalette(buffer);
+            Assert.That(pal.ExtraData, Is.Null, "ExtraData can only exist on full length ACT data");
+            return pal.Colors;
+        }
+
+        [Test]
+        public void Ctor_FullSize_HasExtraData()
+        {
+            var stream = getFullSizeStream();
+            var pal = new ActPalette(stream.ToArray());
+            Assert.That(pal.Colors.Count, Is.EqualTo(ActPalette.MaxColors));
+            if (pal.ExtraData is not null)
+            {
+                Assert.That(pal.ExtraData.Value.NumColors, Is.EqualTo(12345));
+                Assert.That(pal.ExtraData.Value.TransparentIndex, Is.EqualTo(6789));
+            }
+            else
+            {
+                Assert.Fail("ExtraData is null");
+            }
+        }
+
+        [Test]
+        public void Ctor_LargeBuffer_Throws()
+        {
+            var buffer = new byte[ActPalette.MaxLengthInBytes + 1];
+            _ = Assert.Throws<ArgumentException>(() => new ActPalette(buffer));
+        }
+
+        [Test]
+        public void Ctor_Fuzz()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                int bufLen = Random.Shared.Next(ActPalette.MaxLengthInBytes);
+                var buffer = RandomNumberGenerator.GetBytes(bufLen);
+                Assert.DoesNotThrow(() => new ActPalette(buffer));
+            }
+        }
+
+        private static MemoryStream getFullSizeStream()
+        {
+            // build input buffer
+            var stream = new MemoryStream();
+            for (int i = 0; i < ActPalette.MaxColors; i++)
+            {
+                stream.Write(new byte[] { 1, 2, 3 });
+            }
+            var extraData = new ActExtraData()
+            {
+                NumColors = 12345,
+                TransparentIndex = 6789,
+            };
+            unsafe
+            {
+                var span = new Span<byte>(&extraData, sizeof(ActExtraData));
+                stream.Write(span);
+            }
+            return stream;
+        }
+    }
+}
---
+++ test/act2txt.Tests/PaintDotNetTxtPaletteWriterTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+
+namespace act2txt.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class PaintDotNetTxtPaletteWriterTest
+    {
+        [Test]
+        public void WriteColor_WritesColor()
+        {
+            var writer = new StringWriter();
+            var pdnWriter = new PaintDotNetTxtPaletteWriter(writer);
+            pdnWriter.WriteColor(Color.FromArgb(0x10, 0xAB, 0xFF));
+            var result = writer.ToString();
+            Assert.That(result, Is.EqualTo("FF10ABFF\r\n"));
+        }
+
+        [Test]
+        public void WritePalette_WritesAllColors()
+        {
+            var pal = new Color[]
+            {
+                Color.FromArgb(0x10, 0xAB, 0xFF),
+                Color.FromArgb(0x01, 0x02, 0x03),
+            };
+            var writer = new StringWriter();
+            var pdnWriter = new PaintDotNetTxtPaletteWriter(writer);
+            pdnWriter.WritePalette(pal);
+            var result = writer.ToString();
+            Assert.That(result, Is.EqualTo("FF10ABFF\r\nFF010203\r\n"));
+        }
+
+        [Test]
+        public void WriteCommentLine_WritesComment()
+        {
+            var writer = new StringWriter();
+            var pdnWriter = new PaintDotNetTxtPaletteWriter(writer);
+            pdnWriter.WriteCommentLine("test");
+            var result = writer.ToString();
+            Assert.That(result, Is.EqualTo("; test\r\n"));
+        }
+    }
+}
